3736cfd3b6bceaa2d4009e15d1717a9231419469
yusuke.ikeda <yusuke.ikeda@i-studio.co.jp>
2019-10-14 02:12:02 +0900

    ♥

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html＞
-<html>
+<html lang="jp">
 <head>
     <title>どさこの本たち</title>
 </head>
-    <table border = "1">
-    <thead>
-        <tr>
-            <th>本の名前</th>
-            <th>著者名</th>
-        </tr>
-    ＜／thead＞
-    <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
-    </tbody>
-    </table>
+<body>
+<table border="1">
+  <thead>
+      <tr>
+          <th>本の名前</th>
+          <th>著者名</th>
+      </tr>
+  </thaed>
+  <tbody>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+</tbody>
+  </table>  
+</body>
+</html>
