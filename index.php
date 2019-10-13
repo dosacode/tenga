42050c0bb157084ab4eeb70cafc21ec9611fc766
dosacode <d133359@gmail.com>
2019-10-14 00:26:32 +0900

    tenga

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html＞
+<html>
+<head>
+    <title>どさこの本たち</title>
+</head>
+    <table border = "1">
+    <thead>
+        <tr>
+            <th>本の名前</th>
+            <th>著者名</th>
+        </tr>
+    ＜／thead＞
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+    </table>
