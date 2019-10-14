21b0fc3f25635ba758161c45832a9542a0ca5c5a
dosacode <d133359@gmail.com>
2019-10-15 03:17:08 +0900

    second

--- index.php
+++ index.php
@@ -1,19 +1,41 @@
-<!DOCTYPE html＞
-<html>
-<head>
-    <title>どさこの本たち</title>
-</head>
-    <table border = "1">
-    <thead>
-        <tr>
-            <th>本の名前</th>
-            <th>著者名</th>
-        </tr>
-    ＜／thead＞
-    <tbody>
-        <tr>
-            <td></td>
-            <td></td>
-        </tr>
-    </tbody>
-    </table>
+<!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>どさこの本たち</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+
+    <body>
+        <h1>aaaa</h1>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>本の名前</th>
+                    <th>著者名</th>
+                    <th>出版年月日</th>
+                    <th>ページ数</th>
+                    <th>NDC分類(10版)</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>    
+</html>    
