46574482e0be9ba14a649542d99e90dede00e211
yusuke.ikeda <yusuke.ikeda@i-studio.co.jp>
2019-10-19 23:33:03 +0900

    サンプル付与

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html＞
+<!DOCTYPE html>
 <html lang="jp">
 <head>
     <title>どさこの本たち</title>
@@ -18,5 +18,19 @@
     </tr>
 </tbody>
   </table>  
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
 </body>
 </html>
