24e61469441dcd35c69eafe82c3ae43c4d0b1300
yusuke.ikeda <yusuke.ikeda@i-studio.co.jp>
2019-11-16 17:11:40 +0900

    js用

---
+++ assets/js/app-dosacode.js
@@ -0,0 +1,14 @@
+$(function (){
+
+  /**
+  ** 課題 01
+  ** headerの背景色変更するオブジェクト
+  ** @headerColorchange
+  ** @return 返り値はなし
+  **/  
+  function headerColorchange () {
+
+  }
+  headerColorchange()
+
+})
