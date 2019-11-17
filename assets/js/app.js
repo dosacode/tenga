828b6c74687bc4dd105a091ec01a07e1b34cae93
Gits-migii <manicdesign.lab@gmail.com>
2019-11-17 23:38:01 +0900

    ajaxsample
    add

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,8 +39,60 @@ $(function () {
     headingelement.fadeIn(timing)
   }
 
+
+  
   //即時関数で定義してここで実行
   headingFadeIn()
 
+  /**
+  ** 見出しをフェードインさせる
+  ** @textDataGet
+  ** @return なし
+  **/          
 
+  function textDataGet () {
+
+    //ここで空配列を定義
+    var data = [];
+
+    // jQueryのajaxでjsonデータを取得
+    $.ajax({
+      type: 'GET',
+      // url: 'https://spla2.yuu26.com/gachi/now',
+      url:'/assets/js/sample.json',
+      dataType: 'json'
+    })
+    .then(
+
+      // 最初にjsonデータが取ってこれたときの処理を
+      function(json) {
+
+        // for文で繰り返し処理。ここはJavaScriptでjQueryの構文ではない
+        // JSONのresult直下の{},の数だけ繰り返し
+        for(var i = 0; i < json.result.length; i++){
+
+          // 配列にJSONデータを入れていく。
+          //[i]は
+          data.push({
+            'stage': json.result[i].rule,
+            'image': json.result[i].maps_ex[0].image
+          })
+        }
+        
+        // dataに入った配列のお数だけfor文で繰り返し処理。ここはJavaScriptでjQueryの構文ではない
+        for(var i = 0; i < data.length; i++){
+          console.log('今のスプラトゥーンのガチマッチは' + data[i]['stage'] + 'です。')
+          console.log(data[i]['image'])
+        }
+      },
+
+      // jsonが読み込めなかった時のエラー時の処理
+      // urlを空にするなどするとこちらの処理に切り替わります。
+      function() {
+        console.log('読み込みに失敗しました');
+      }
+    )
+   }
+   textDataGet()
+  
 })
